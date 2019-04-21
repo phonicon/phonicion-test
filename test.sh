d094a0ef5de499c12a9ab742012f592e67db2d63
Orish <775504+Orad@users.noreply.github.com>
2019-04-21 11:00:15 +0300

    wip

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "test"
 env
+
+echo "------------------"
+if [ "${PHONIC_AWS}" == "GLOBAL" ]
+then
+	echo "GLOBAL"
+elif [[ "${PHONIC_AWS}" == "STAGE" ]]; then
+	echo "STAGE"
+fi
+
